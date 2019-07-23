37c6baa92e476962dcc6a14bce258625d990f026
Simon Burton <burt76@outlook.com>
2019-07-23 15:01:05 +0100

    inital load

---
+++ ConsoleUi.Tests/PersonTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+
+
+namespace ConsoleUi.Tests
+{
+    [TestFixture]
+    public class PersonTests
+    {
+
+        [Test]
+        public void ShouldReturnFirstNameLastNameAsString()
+        {
+            Assert.AreEqual("Simon.Burton", TestPerson().ToString());
+        }
+
+        [Test]
+        public void ShouldReturnFalseFirstNameLastNameAsString()
+        {
+            Assert.AreNotEqual("SimonBurton", TestPerson().ToString());
+        }
+
+        [Test]
+        public void ShouldReturnAgeFromAgeInYears()
+        {
+            var person = TestPerson();
+            var result = person.AgeOnNextBirthday();
+
+            Assert.AreEqual(43, result);
+        }
+
+
+
+        public static Person TestPerson()
+        {
+            return new Person
+            { FirstName = "Simon", LastName = "Burton", Dob = new DateTime(1976, 11, 14) };
+        }
+    }
+}
---
+++ ConsoleUi.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ConsoleUi.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleUi.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0c58fa35-890f-448a-9e7f-7fcb49069ecf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
